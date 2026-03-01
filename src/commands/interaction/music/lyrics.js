901a5c3da913526408232304d30f0715bc91c3b4
KoDdy <akshaykb0101@gmail.com>
2026-03-01 14:42:26 +0530

    live lyrics command added
    Live lyrics

--- src/commands/interaction/music/lyrics.js
+++ src/commands/interaction/music/lyrics.js
@@ -62,3 +62,13 @@ module.exports = {
         }, 5000);
     }
 };
+
+/**
+ * Project: Nexa Music
+ * Author: KoDdy, Razi
+ * Company: Infinity
+ * This code is the property of Infinity and may not be reproduced or
+ * modified without permission. For more information, contact us at
+ * https://discord.gg/fbu64BmPFD
+
+ */
