fca932937956d905a85f8c5726e797844e618316
raziscofield <rajihraju11@gmail.com>
2026-03-03 18:18:39 +0530

    updated config js with guide

--- src/settings/config.js
+++ src/settings/config.js
@@ -1,22 +1,19 @@
-module.exports = {
-    token: "-",
-    clientid: "-",
-    prefix: "-",
-    engine: "ytsearch",
-    color: "#FF0000",
-    mongodburl: "-",
-    geniusToken: "-",
-    developers: ["-"],
-    sharding: false,
-    database: false,
-    invite: "-",
+module.exports = { 
+    token: "-",  //token of your bot, get it from https://discord.com/developers/applications
+    clientid: "-", //client id of your bot, get it from https://discord.com/developers/applications
+    prefix: "-", //prefix of your bot, for example "!" or "n!"
+    engine: "ytsearch", //search engine for the play command, options: "ytsearch", "spotify", "soundcloud", "deezer", "applemusic"
+    color: "#FF0000", //color of the ui, in hex format
+    mongodburl: "", //mongodb url for the dashboard, if you don't want to use the dashboard, you can leave it as an empty string
+    geniusToken: "", //genius token for lyrics command, get it from https://genius.com/developers
+    developers: [""], //array of developer ids, these users will have access to the developer commands and the dashboard
     nodes: [
         {
-            name: "nexa-1",
-            host: "localhost",
-            password: "password",
-            port: 457,
-            secure: false,
+            name: "nexa-1", //name of the node, can be anything
+            host: "localhost", //host of the lavalink server, if it's on the same machine as the bot, you can use "localhost"
+            password: "youshallnotpass", //password of the lavalink server, get it from your lavalink config
+            port: 2000, //port of the lavalink server, get it from your lavalink config
+            secure: false, //whether the lavalink server is secure (https) or not (http), get it from your lavalink config
         },
     ]
 }
